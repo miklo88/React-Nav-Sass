411e0959ad4a97c520949c33c15d9cbc906e1e41
Carlos <miklo.concepcion@gmail.com>
2019-12-29 16:58:33 -0500

    cleaning up my styles scss folder. making styling code more dry for the simple react component.

--- react-nav/src/utils/routing.js
+++ react-nav/src/utils/routing.js
@@ -10,26 +10,31 @@ const Routes = () => {
   return (
     <BrowserRouter>
       <div className='nav-main'>
-        <div className='nav-h1'>
-          <h1>ReactJS</h1>
+        {/* nav h1 title */}
+        <h1 className='nav-h1'>ReactJS</h1>
+        {/* navigation buttons and links and routes */}
+        <div className='nav-btn-wrap'>
+          <button className='nav-button'>
+            <Link className='btn-link' to='/home'>
+              <p>Landing Page</p>
+            </Link>
+          </button>
+          <button className='nav-button'>
+            <Link className='btn-link' to='/about'>
+              <p className='btn-text'>About</p>
+            </Link>
+          </button>
+          <button className='nav-button'>
+            <Link className='btn-link' to='/contact'>
+              <p className='btn-text'>Contact</p>
+            </Link>
+          </button>
         </div>
-        <button className='nav-button'>
-          <Link className='link' to='/home'>
-            <p className='btn-text'>Landing Page</p>
-          </Link>
-        </button>
-        <button className='nav-button'>
-          <Link className='link' to='/about'>
-            <p className='btn-text'>About</p>
-          </Link>
-        </button>
-        <button className='nav-button'>
-          <Link className='link' to='/contact'>
-            <p className='btn-text'>Contact</p>
-          </Link>
-        </button>
+        {/* nav-btn-wrap */}
       </div>
+      {/* nav-main */}
 
+      {/* route paths */}
       <Switch>
         <Route path='/home' component={Home} />
         <Route path='/about' component={About} />
