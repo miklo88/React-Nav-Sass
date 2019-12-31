f0e2336ec4524c6e9c5ed6ad0f1a7ab7c4d047ae
Carlos <miklo.concepcion@gmail.com>
2019-12-30 21:41:36 -0600

    updated dev dependencies. used npm rebuild node-sass. never used that before. updated env when you havent in long enough. adding my styles to this biz. can't wait to finish that form.

--- react-nav/src/utils/footer.js
+++ react-nav/src/utils/footer.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+// import React from "react";
+// import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
-// footer body
-const Footer = () => {
-  return (
-    <BrowserRouter>
-      <div className='footer-body'>
-        {/* homepage button */}
-        <Link className='back-home' to='/home'>
-          Home
-        </Link>
-        {/* <Switch>
-          <Route path='/home' component={Home} />
-        </Switch> */}
-      </div>
-    </BrowserRouter>
-    // footer-body
-  );
-};
+// // footer body
+// const Footer = () => {
+//   return (
+//     <BrowserRouter>
+//       <div className='footer-body'>
+//         {/* homepage button */}
+//         <Link className='back-home' to='/home'>
+//           Home
+//         </Link>
+//         {/* <Switch>
+//           <Route path='/home' component={Home} />
+//         </Switch> */}
+//       </div>
+//     </BrowserRouter>
+//     // footer-body
+//   );
+// };
 
-export default Footer;
+// export default Footer;
