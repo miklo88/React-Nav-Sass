78553cdbefbd11c9eee4d2fd40ebe7b5bc3524e1
Carlos <miklo.concepcion@gmail.com>
2020-01-04 23:02:45 -0600

    re-awakening the project. going to play with implementing an api on the front end with axios.

--- react-nav/src/App.js
+++ react-nav/src/App.js
@@ -8,6 +8,18 @@ import "./styles/variables.scss";
 // importing components
 import Routes from "./utils/routing";
 // import Footer from "./utils/footer";
+function a() {
+  console.log("result of a()");
+}
+function b() {
+  console.log("result of b()");
+}
+function c() {
+  console.log("result of c()");
+}
+console.log("a() is done!");
+console.log("b() is done!");
+console.log("c() is done!");
 
 function App() {
   return (
